097848b66204be39256f84fc7872491a8cb123dc
ymbrite <132761445+ymbrite@users.noreply.github.com>
2025-12-28 21:24:42 -0800

    Update posts.ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -26,7 +26,7 @@ export async function getPostData(id: any) {
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
-    .use(html)
+    .use(html, { sanitize: false })
     .use(remarkGfm)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
